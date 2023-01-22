72737330ec3526b6e82a8129ef5dc00e6eda3f6a
Bryan Vera <brayanandresveragar@gmail.com>
2023-01-22 16:38:21 -0500

    :triangular_flag_on_post: Inicialice mi proyecto de alura "Encriptador"
    Se realizaron las siguientes modificaciones

---
+++ script.js
@@ -0,0 +1,72 @@
+const textArea = document.querySelector(".programa__cuadro-texto")
+const mensaje = document.querySelector(".programa__cuadro-texto--salida")
+const botonEncriptar = document.getElementById("btn-encriptador")
+const botonDesencriptar = document.getElementById("btn-desencriptador")
+const decoracion = document.querySelector(".decoracion")
+const btnCopiar = document.getElementById("boton-copiar")
+var answer = document.getElementById("respuestaCopiado");
+
+botonEncriptar.addEventListener("click",btnEncriptar)
+botonDesencriptar.addEventListener("click",btnDesencriptar)
+
+btnCopiar.addEventListener('click', copiar);
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+
+    decoracion.style.display = "none"
+    mensaje.style.display = "block"
+    mensaje.value = textoEncriptado
+    textArea.value = ""
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = ""
+}
+
+function copiar() {
+    // Sleccionando el texto
+    mensaje.select(); 
+    try {
+        // Copiando el texto seleccionado
+        var successful = document.execCommand('copy');
+  
+        if(successful) answer.innerHTML = 'Copiado!';
+        else answer.innerHTML = 'Incapaz de copiar!';
+    } catch (err) {
+        answer.innerHTML = 'Browser no soportado!';
+    }
+    mensaje.value = ""
+}
+
+function encriptar(stringEncriptado) {
+    let matrisCodigo = [["e","enter"],["i","amis"],["a","ai"],["o","ober"],["u","ufat"]]
+    console.table(matrisCodigo)
+    
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for (let i = 0; i<matrisCodigo.length; i++) {
+        if(stringEncriptado.includes(matrisCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrisCodigo[i][0],matrisCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
+function desencriptar(stringDesencriptado) {
+    let matrisCodigo = [["e","enter"],["i","amis"],["a","ai"],["o","ober"],["u","ufat"]]
+    console.table(matrisCodigo)
+    
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for (let i = 0; i<matrisCodigo.length; i++) {
+        if(stringDesencriptado.includes(matrisCodigo[i][0])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrisCodigo[i][1],matrisCodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+
+
